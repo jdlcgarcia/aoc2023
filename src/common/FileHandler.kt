7daf4acb1208c282b82f19a4b7caeabc552321e9
Juan Garcia <jcruzgar@gmail.com>
2023-12-01 18:28:19 +0100

    day01: new method to print in files, mostly to test

--- src/common/FileHandler.kt
+++ src/common/FileHandler.kt
@@ -1,6 +1,15 @@
 package common
 
+import java.nio.file.StandardOpenOption
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.io.path.writeText
 
 fun readInput(name: String) = Path("src/$name/input.txt").readLines()
+
+fun append(day: String, name: String, line: String) = Path("src/$day/$name").writeText(
+    line,
+    Charsets.UTF_8,
+    StandardOpenOption.CREATE,
+    StandardOpenOption.APPEND
+)
