cc47bfee4bfb1dca1fc305cbd8ebb83690fd11a7
Juan Garcia <jcruzgar@gmail.com>
2023-12-01 18:27:00 +0100

    day01: second part solved. Could be prettier, tho.

--- src/day01/Solution.kt
+++ src/day01/Solution.kt
@@ -1,5 +1,6 @@
 package day01
 
+import common.append
 import common.readInput
 
 fun main() {
@@ -8,7 +9,8 @@ fun main() {
     var sum = 0
 
     for (line in testInput) {
-        sum += searchFirstAndLast(line)
+//        sum += searchFirstAndLast(line)
+        sum += searchFirstAndLastWithSpelling(line)
     }
 
     println("What is the sum of all of the calibration values?")
@@ -30,13 +32,91 @@ fun searchFirstAndLast(line: String): Int {
     return (firstDigit + lastDigit).toInt()
 }
 
+fun searchFirstAndLastWithSpelling(line: String): Int {
+    val first = searchFirstFromLeft(line)
+    val last = searchFirstFromRight(line)
+
+    return first * 10 + last
+}
+
+fun searchFirstFromRight(line: String): Int {
+    val digitMap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    var index = line.length -1
+    while (index > 0) {
+        if (line[index].isDigit()) {
+            return line[index].digitToInt()
+        }
+
+        for (digit in digitMap.keys) {
+            if (index + digit.length <= line.length) {
+                val substringToCheck = line.substring(index, index + digit.length)
+                if (substringToCheck == digit) {
+                    return digitMap[digit]!!
+                }
+            }
+        }
+
+        index--
+    }
+
+    return line[0].digitToInt();
+}
+
+fun searchFirstFromLeft(line: String): Int {
+    val digitMap = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
+    var index = 0
+    while (index < line.length) {
+        if (line[index].isDigit()) {
+            return line[index].digitToInt()
+        }
+
+        for (digit in digitMap.keys) {
+            if (index + digit.length <= line.length) {
+                val substringToCheck = line.substring(index, index + digit.length)
+                if (substringToCheck == digit) {
+                    return digitMap[digit]!!
+                }
+            }
+        }
+
+        index++
+    }
+
+    return 0;
+}
+
 
 fun loadTestInput(): Array<String> {
     val multilineString = """
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
     """.trimIndent()
 
     // Split the multiline string into an array of strings
