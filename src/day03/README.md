79d6320ca0dfd97b8ec301dff7df5a4b1b2a2d77
Juan Garcia <jcruzgar@gmail.com>
2023-12-03 15:49:06 +0100

    day03: done with solution of second part. hacky af.

--- src/day03/README.md
+++ src/day03/README.md
@@ -15,4 +15,6 @@ Happy coding!
 
 ## Solution
 
-This first part went wild! Reading the second part I think I did a bad approach... I parsed the numbers and, when I had a complete number, I checked the surrounding cells to see if there was a character other than a dot or a digit. Maybe I should have searched for symbols and then find the number surrounding them... so probably I will make that for the second part.  
+This first part went wild! Reading the second part I think I did a bad approach... I parsed the numbers and, when I had a complete number, I checked the surrounding cells to see if there was a character other than a dot or a digit. Maybe I should have searched for symbols and then find the number surrounding them... so probably I will make that for the second part.
+
+Loved the recursive solution to find the first character of the number! A quién no le va a gustá una función recursiva de las que se dan en primero? It's been a little bit hacky, ngl, but having the frustration of making the opposite approach on the first part, it's more than good enough for me
--- src/day03/Solution.kt
+++ src/day03/Solution.kt
@@ -1,6 +1,8 @@
 package day03
 
 import common.readInput
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val testInput = readInput("day03")
@@ -13,6 +15,71 @@ fun main() {
     }
 
     clearNonPartNumbers(engine)
+    findGear(engine)
+}
+
+fun findGear(engine: MutableList<String>) {
+    var rowIndex = 0
+    var sumOfGearRatios = 0
+
+    while (rowIndex < engine.size) {
+        sumOfGearRatios += findGearInRow(engine, rowIndex)
+        rowIndex++
+    }
+
+    println("What is the sum of all of the gear ratios in your engine schematic?")
+    println(sumOfGearRatios.toString())
+}
+
+fun findGearInRow(engine: MutableList<String>, rowIndex: Int): Int {
+    var colIndex = 0
+    var sumOfGearRatiosPerLine = 0
+
+    while (colIndex < engine[rowIndex].length) {
+        if (engine[rowIndex][colIndex] == '*') {
+            sumOfGearRatiosPerLine += scanForNumberThatSurroundsSymbol(engine, rowIndex, colIndex)
+        }
+        colIndex++
+    }
+
+    return sumOfGearRatiosPerLine
+}
+
+private fun scanForNumberThatSurroundsSymbol(engine: MutableList<String>, rowIndex: Int, colIndex: Int): Int {
+    var numbersAroundSymbol: MutableList<Int> = mutableListOf()
+    for(rowNumber in max(0, rowIndex-1)..min(rowIndex +1, engine.size -1)) {
+        var colNumber = max(0, (colIndex-1))
+        while (colNumber <= min((colIndex +1), engine[rowIndex].length -1)) {
+            if (!(rowNumber == rowIndex && colNumber == colIndex) && engine[rowNumber][colNumber].isDigit()) {
+                val number = findCompleteNumber(engine, rowNumber, colNumber)
+                numbersAroundSymbol.add(number)
+                while (engine[rowNumber][colNumber].isDigit() && colNumber < min((colIndex +1), engine[rowIndex].length -1)) {
+                    colNumber++
+                }
+            }
+            colNumber++;
+        }
+    }
+
+    if (numbersAroundSymbol.size == 2) {
+        return numbersAroundSymbol.reduce { acc, item -> acc * item }
+    }
+
+    return 0;
+}
+
+private fun findCompleteNumber(
+    engine: MutableList<String>,
+    rowNumber: Int,
+    colNumber: Int
+): Int {
+    //check at the left: if it's index 0, it's the beginning. If it's dot, it's also the beginning
+    if (colNumber == 0 || !engine[rowNumber][colNumber-1].isDigit()) {
+        val foundNumberInfo = findFullNumber(engine, rowNumber, colNumber)
+        return parseNumber(engine, rowNumber, foundNumberInfo)
+    }
+    // if cell at left it's a number, recursive call!!
+    return findCompleteNumber(engine, rowNumber, colNumber-1)
 }
 
 fun clearNonPartNumbers(engine: MutableList<String>) {
@@ -37,7 +104,7 @@ private fun processLine(
     while (colIndex < engine[rowIndex].length) {
         if (engine[rowIndex][colIndex].isDigit()) {
             val foundNumberInfo = findFullNumber(engine, rowIndex, colIndex)
-            val number = engine[rowIndex].substring(foundNumberInfo["start"]!!, (foundNumberInfo["end"]!! +1)).toInt()
+            val number = parseNumber(engine, rowIndex, foundNumberInfo)
             if (findSymbolSurroundingNumber(engine, rowIndex, foundNumberInfo)) {
                 sumOfPartNumbersPerLine += number
             }
@@ -50,18 +117,20 @@ private fun processLine(
     return sumOfPartNumbersPerLine
 }
 
+private fun parseNumber(
+    engine: MutableList<String>,
+    rowIndex: Int,
+    foundNumberInfo: MutableMap<String, Int>
+) = engine[rowIndex].substring(foundNumberInfo["start"]!!, (foundNumberInfo["end"]!! + 1)).toInt()
+
 fun findSymbolSurroundingNumber(engine: MutableList<String>, rowIndex: Int, foundNumberInfo: MutableMap<String, Int>): Boolean {
     for(rowNumber in (rowIndex-1)..(rowIndex +1)) {
         if (rowNumber in 0..<engine.size) {
             for(columnNumber in (foundNumberInfo["start"]!! -1)..(foundNumberInfo["end"]!! +1)) {
                 if (columnNumber in 0..<engine[0].length) {
-//                    if (rowNumber == rowIndex && columnNumber !in foundNumberInfo["start"]!!..foundNumberInfo["end"]!!) {
-//                        println("row "+rowNumber+", col "+columnNumber + ": " + engine[rowNumber][columnNumber])
-                        if (!engine[rowNumber][columnNumber].isDigit() && engine[rowNumber][columnNumber] != '.') {
-                            return true;
-                        }
-//                    }
-
+                    if (!engine[rowNumber][columnNumber].isDigit() && engine[rowNumber][columnNumber] != '.') {
+                        return true;
+                    }
                 }
             }
         }
@@ -87,16 +156,16 @@ fun findFullNumber(engine: MutableList<String>, rowIndex: Int, colIndex: Int): M
 
 fun loadTestInput(): Array<String> {
     val multilineString = """
-467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..
+12.34.....
+..*....*..
+..........
+..........
+...526.912
+......*...
+..........
+..........
+..........
+..........
     """.trimIndent()
 
     // Split the multiline string into an array of strings
