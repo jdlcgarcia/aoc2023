3199771d3c0b178567e22d91076121b423cf5e12
Juan Garcia <jcruzgar@gmail.com>
2023-12-06 21:44:58 +0100

    day06: both parts done!

---
+++ src/day06/README.md
@@ -0,0 +1,19 @@
+# Day 06
+
+This directory contains the Kotlin solution and input data for Day 06 of Advent of Code 2023.
+
+## Instructions
+
+1. Implement your solution in `Solution.kt`.
+2. Build and run your solution using the provided Makefile target:
+
+   ```
+   make run06
+   ```
+
+Happy coding!
+
+## Solution
+
+I'm in shock, it was too easy! even with brute force, I could make the two parts without much effort :) it's a sweet sensation after the sour one from yesterday!!
+
---
+++ src/day06/Solution.kt
@@ -0,0 +1,19 @@
+package day06
+
+fun main() {
+    var countVictories =
+//        Race(7, 9).getVictories() *
+//        Race(15, 40).getVictories() *
+//        Race(30, 200).getVictories()
+//        Race(40, 215).getVictories() *
+//        Race(70, 1051).getVictories() *
+//        Race(98, 2147).getVictories() *
+//        Race(79, 1005).getVictories()
+//        Race(71530, 940200).getVictories()
+        Race(40709879, 215105121471005).getVictories()
+
+    println("What do you get if you multiply these numbers together?")
+    println(countVictories)
+}
+
+
